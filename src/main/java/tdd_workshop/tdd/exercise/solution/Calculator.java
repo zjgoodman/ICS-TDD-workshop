83e93baf9c4b848cb24ba91adf0075d7cfd789df
Zach Goodman <zachary.goodman@anthem.com>
2024-04-01 10:29:21 -1000

    subtract

--- src/main/java/tdd_workshop/tdd/exercise/solution/Calculator.java
+++ src/main/java/tdd_workshop/tdd/exercise/solution/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
         return input1 + input2;
     }
 
+    public static int subtract(int input1, int input2) {
+        return input1 - input2;
+    }
+
 }
--- src/test/java/tdd_workshop/tdd/exercise/solution/CalculatorTests.java
+++ src/test/java/tdd_workshop/tdd/exercise/solution/CalculatorTests.java
@@ -14,6 +14,14 @@ public class CalculatorTests {
         runTest(Calculator::add, 0, 0, 0);
     }
 
+    @Test
+    public void testSubtract() {
+        runTest(Calculator::subtract, 1, 1, 0);
+        runTest(Calculator::subtract, 1, 2, -1);
+        runTest(Calculator::subtract, -1, -2, 1);
+        runTest(Calculator::subtract, 10, 2, 8);
+    }
+
     private void runTest(BiFunction<Integer, Integer, Integer> function, int input1, int input2, int expectedResult) {
         int actualResult = function.apply(input1, input2);
         Assertions.assertEquals(expectedResult, actualResult);
