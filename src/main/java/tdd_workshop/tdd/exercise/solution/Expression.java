24ed1d1083178b15231c2f86a30884fa91a0af5c
Zach Goodman <zachary.goodman@anthem.com>
2024-04-03 08:08:15 -1000

    fix multi digit bug

--- src/main/java/tdd_workshop/tdd/exercise/solution/Expression.java
+++ src/main/java/tdd_workshop/tdd/exercise/solution/Expression.java
@@ -3,6 +3,10 @@ package tdd_workshop.tdd.exercise.solution;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.function.BiFunction;
 
 
@@ -14,14 +18,32 @@ public class Expression {
     public int input1;
     public int input2;
 
+    private static final Set<Character> DELIMITERS = Set.of('+','-','*','/');
+
     public Expression(String expression){
-        var array = expression.toCharArray();
-        int input1 = Integer.parseInt(String.valueOf(array[0]));
-        char operation = array[1];
-        int input2 = Integer.parseInt(String.valueOf(array[2]));
+        List<String> strings = splitString(expression, DELIMITERS);
+        int input1 = Integer.parseInt(strings.get(0));
+        char operation = strings.get(1).charAt(0);
+        int input2 = Integer.parseInt(strings.get(2));
         this.operator = operation;
         this.input1 = input1;
         this.input2 = input2;
+    }
+
+    public static List<String> splitString(String input, Set<Character> delimiters) {
+        List<String> strings = new LinkedList<>();
+        StringBuilder builder = new StringBuilder();
+        for (char character : input.toCharArray()) {
+            if (delimiters.contains(character)) {
+                strings.add(builder.toString());
+                strings.add(String.valueOf(character));
+                builder = new StringBuilder();
+            } else {
+                builder.append(character);
+            }
+        }
+        strings.add(builder.toString());
+        return strings.stream().filter(string -> !string.isEmpty()).toList();
     }
 
     public int evaluate() {
--- src/test/java/tdd_workshop/tdd/exercise/solution/ExpressionTests.java
+++ src/test/java/tdd_workshop/tdd/exercise/solution/ExpressionTests.java
@@ -3,22 +3,49 @@ package tdd_workshop.tdd.exercise.solution;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Set;
+
 public class ExpressionTests {
     @Test
     public void testParseExpression() {
         String inputString = "1+1";
-        Expression expectedExpression = new Expression('+',1,1);
+        Expression expectedExpression = new Expression('+', 1, 1);
+        Expression actualExpression = new Expression(inputString);
+        Assertions.assertEquals(expectedExpression, actualExpression);
+    }
+    @Test
+    public void testParseMultiDigitExpression() {
+        String inputString = "123+456";
+        Expression expectedExpression = new Expression('+', 123, 456);
+        Expression actualExpression = new Expression(inputString);
+        Assertions.assertEquals(expectedExpression, actualExpression);
+    }
+    @Test
+    public void testMultiOperationExpressionGetsTruncated() {
+        String inputString = "123+456+789";
+        Expression expectedExpression = new Expression('+', 123, 456);
         Expression actualExpression = new Expression(inputString);
         Assertions.assertEquals(expectedExpression, actualExpression);
     }
 
     @Test
-    public void testEvaluate(){
-        Expression expression = new Expression('+',1,2);
+    public void testSplitString() {
+        String inputString = "12345+67890+1+45";
+        Set<Character> delimiters = Set.of('+');
+        var expectedSplit = List.of("12345","+","67890","+","1","+","45");
+        var actualSplit = Expression.splitString(inputString, delimiters);
+        Assertions.assertEquals(expectedSplit, actualSplit);
+    }
+
+    @Test
+    public void testEvaluate() {
+        Expression expression = new Expression('+', 1, 2);
         int expectedResult = 3;
         int actualResult = expression.evaluate();
-        Assertions.assertEquals(expectedResult,actualResult);
+        Assertions.assertEquals(expectedResult, actualResult);
     }
+
     @Test
     public void testEvaluateFromString() {
         runEvaluateTest("1+1", 2);
@@ -32,10 +59,11 @@ public class ExpressionTests {
         runEvaluateTest("4/2", 2);
         runEvaluateTest("7/2", 3);
     }
-    private void runEvaluateTest(String expression, int expectedResult){
+
+    private void runEvaluateTest(String expression, int expectedResult) {
         Expression expressionObject = new Expression(expression);
         int actualResult = expressionObject.evaluate();
-        Assertions.assertEquals(expectedResult,actualResult);
+        Assertions.assertEquals(expectedResult, actualResult);
     }
 
     @Test
